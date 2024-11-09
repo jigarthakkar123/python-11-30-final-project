61313225bc3535bc54542cb8b6bfcdf7555778be
Jigar Thakkar <104816389+jigarthakkar123@users.noreply.github.com>
2024-11-09 14:32:05 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!qrj1nr&uz)ce6!m!7i=n26l&c#-nzq0wwbpey@h7)rnvzt6os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','.pythonanywhere.com','python-11-30-final-project.onrender.com']
 
 
 # Application definition
