e5beb096a24919acf9eb5b4ecbeb1f3f79f2b2c0
Jigar Thakkar <104816389+jigarthakkar123@users.noreply.github.com>
2024-11-09 16:20:44 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -68,7 +69,7 @@ TEMPLATES = [
         },
     },
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
